3c2fd3107b2b7f142ef4348953a41dca7fcadc72
faimakhalid <fatimakhalidjutt@gmail.com>
2025-01-18 12:08:22 +0500

    Add files via upload

---
+++ chap 3/script.js
@@ -0,0 +1,24 @@
+// Question no 1
+
+let age = 15; // Apni age yahan daalein
+alert("I am " + age + " years old");
+
+// Question no 2
+
+let visitCount = 14; // Visit count initialize karen
+alert("You have visited this site " + visitCount + " times");
+
+// Question no 3
+
+let birthYear = 1990; // Yahan apna birth year daalein
+        document.write("My birth year is " + birthYear + "<br>");
+        document.write("Data type of my declared variable is " + typeof birthYear);
+
+// Question no 4
+  // Variables declare karein
+  let visitorName = "John Doe"; // Visitor ka naam
+  let productTitle = "T-shirt(s)"; // Product ka naam
+  let quantity = 5; // Quantity jo order ki gayi hai
+
+  // Message browser mein display karein
+  document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store.");
